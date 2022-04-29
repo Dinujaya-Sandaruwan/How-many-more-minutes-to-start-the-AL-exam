f07972d1c478521551d3a07c0944db82c2ca6f29
Dinujaya-Sandaruwan <itkindom.dhomes@gmail.com>
2022-04-30 01:02:13 +0530

    Done.

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,27 @@
 const countdown = () => {
-    const countDate = new Date('October 17, 2022 08:00:00')
-    console.log(countDate)
+    const countDate = new Date('October 17, 2022 08:00:00').getTime();
+    const now = new Date().getTime();
+    // console.log(countDate);
+    // console.log(now);
+
+    const gap = countDate - now;
+    const secoud = 1000;
+    // console.log(gap);
+
+    var time_ele = document.getElementById('count');
+    var main_time = Math.floor(gap / secoud)
+    var string_time = main_time.toLocaleString("en-US")
+    time_ele.innerText = string_time + ' SECONDS'
+
 }
 
-countdown()
+setInterval(countdown, 1000)
+
+
+// Background Music
+
+var audio = new Audio('audio/lofi.mp3');
+audio.loop = true;
+
+
+
